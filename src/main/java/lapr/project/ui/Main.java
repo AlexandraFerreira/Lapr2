b9bf6f789060cef4c4fb940bdb49583e31d9652f
Jose Lucas <1140871@isep.ipp.pt>
2016-06-01 12:51:21 +0100

    Criação do controler DefinirFAE (Incompleto).

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,6 +1,7 @@
 package lapr.project.ui;
 
 import lapr.project.model.CalculatorExample;
+import java.util.Scanner;
 
 /**
  * @author Nuno Bettencourt <nmb@isep.ipp.pt> on 24/05/16.
@@ -18,9 +19,17 @@ class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-
+            Scanner ler = new Scanner(System.in);
+            int cont;
+            char resposta;
 		CalculatorExample calculatorExample = new CalculatorExample();
 		System.out.println(calculatorExample.sum(3, 5));
+                //UC2
+                /*do{
+                    
+                    System.out.println("Continuar a introduzir?");
+                    resposta = ler.next();
+                }while(resposta == 's' || resposta == 'S')*/
 	}
 
 }
