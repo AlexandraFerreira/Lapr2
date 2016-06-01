4f531ce46547747cd429b73604faefa4b2f7afc4
Miguel Ângelo Lopes de Oliveira <1140616@isep.ipp.pt>
2016-06-01 11:02:01 +0100

    Add class Decisao [UC4] [Implementation of  methods like,  (getInfoDecisao,setDecisao(), setJustificacao(), valida())]
    Add class DecidirCandidaturasController [UC4] [Implementation of 
    methods like (getListaCandidaturasExposicao(), getListaCandidaturasPorDecidir(fae), setDecisao(d,justificacao), registaDecisao(), getInformacaoCandidaturaPorDecidir(fae))]

---
+++ src/main/java/lapr/project/controller/DecidirCandidaturasController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.*;
+
+/**
+ *
+ * @author Miguel-PC
+ */
+public class DecidirCandidaturasController {
+
+    /**
+     * Método que vai retornar a lista de candidaturas a uma determinada
+     * exposição
+     *
+     * @return a lista de candidaturas a uma determinada exposicao
+     */
+    public List<Candidatura> getListaCandidaturasExposicao() {
+
+    }
+
+    /**
+     * Método que vai retornar a lista de candidaturas por decidir de um
+     * determinado FAE
+     *
+     * @param fae do tipo FAE (funcionario de apoio à exposição)
+     * @return lista de candidaturas por decidir de um FAE
+     */
+    public List<Candidatura> getListaCandidaturasPorDecidir(FAE fae) {
+
+    }
+
+    /**
+     * Método que sempre que um FAE tomar uma decisao, vai guardar no sistema a
+     * decisao (se candidatura é aceite ou não) e a respectiva justificação para
+     * tal decisao
+     *
+     * @param d do tipo Decisao - Decisao tomada pelo FAE à candidatura a uma
+     * exposicao lhe atribuida
+     * @param justificacao do tipo String - Texto justificativo apresentando o
+     * porquê de tal decisao para cada candidatura a uma exposicao
+     */
+    public void setDecisao(Decisao d, String justificacao) {
+
+    }
+
+    /**
+     * Método que vai fazer o registo da decisao tomada pelo FAE a uma
+     * determinada candidatura lhe atribuida
+     *
+     * @return registo da decisao (decisao fica guardada no sistema)
+     */
+    public Decisao registaDecisao() {
+
+    }
+
+    /**
+     * Método que retorna a informacao da candidatura ainda por decidir pelo FAE
+     *
+     * @param fae do tipo FAE (funcionario de apoio à exposição)
+     * @return informação da candidatura por decidir
+     */
+    public String getInformacaoCandidaturaPorDecidir(FAE fae) {
+
+    }
+}
---
+++ src/main/java/lapr/project/model/Decisao.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+/**
+ *
+ * @author Miguel-PC
+ */
+public class Decisao {
+
+    /**
+     * Método que vai retornar a informação de uma decisão
+     *
+     * @param d do tipo Decisao - Decisao tomada pelos FAE a uma candidatura de
+     * uma certa exposicao
+     * @return informação da decisao (String)
+     */
+    public String getInfoDecisao(Decisao d) {
+
+    }
+
+    /**
+     * Método que modifica a decisao
+     *
+     * @param d do tipo Decisao (decisao tomada pelos FAE a uma candidatura)
+     */
+    public void setDecisao(Decisao d) {
+
+    }
+
+    /**
+     * Método que modifica a justificação dada pelo FAE para uma decisao
+     *
+     * @param justificacao do tipo String (justificação dada pelo FAE para
+     * decisao de candidaturas)
+     */
+    public void setJustificacao(String justificacao) {
+
+    }
+
+    /**
+     * Método de validação
+     *
+     * @return
+     */
+    public boolean valida() {
+
+    }
+
+    /**
+     * Método que vai adicionar uma decisão à lista de decisões
+     *
+     * @param d do tipo Decisao (decisao tomada pelo FAE para uma candidatura)
+     * @return decisao fica guardada na lista de decisões
+     */
+    public boolean add(Decisao d) {
+
+    }
+}
