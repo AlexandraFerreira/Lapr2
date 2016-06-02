e1b6f3957d7d2c2c29ff6e0c547771fe19bfdd26
Cristiano Melo <1140869@isep.ipp.pt>
2016-06-02 10:38:01 +0100

    Update class Candidatura used on UC3 [ getter and setter, constructor and javadoc ].

--- src/main/java/lapr/project/model/Candidatura.java
+++ src/main/java/lapr/project/model/Candidatura.java
@@ -33,6 +33,15 @@ public class Candidatura {
     
     private Produto produto;
     
+    private static double AREA_POR_OMISSAO=0;
+    
+    private static int QUANT_CONVITES_POR_OMISSAO=0;
+    
+    private static String NOME_EMPRESA_POR_OMISSAO= "Sem nome";
+    
+    private static String MORADA_POR_OMISSAO="Sem morada";
+    
+    private static int TELEFONE_POR_OMISSAO=220000000;
     
     /**
      * Construtor completo que inicia a Candidatura com todos os elementos da classe
@@ -49,11 +58,90 @@ public class Candidatura {
         this.nomeEmp=nomeEmp;
         this.morada=morada;
         this.telefone=telefone;
-       
-        
+    }
+    /**
+     * Construtor vazio de Candidatura que inicia as variaveis por omissao.
+     */
+          public Candidatura(){
+              this.areaPretendida=AREA_POR_OMISSAO;
+              this.quatConvites=QUANT_CONVITES_POR_OMISSAO;
+              this.nomeEmp=NOME_EMPRESA_POR_OMISSAO;
+              this.morada=MORADA_POR_OMISSAO;
+              this.telefone=TELEFONE_POR_OMISSAO;
+          }
+/**
+ * Metodo getAreaPretendida que retorna a area pretendida.
+ * @return 
+ */
+    public double getAreaPretendida() {
+        return areaPretendida;
+    }
+/**
+ * Metodo setAreaPretendida que permite alterar a area pretendida.
+ * @param areaPretendida 
+ */
+    public void setAreaPretendida(double areaPretendida) {
+        this.areaPretendida = areaPretendida;
+    }
+/**
+ * Metodo getQuantConvites que retorna a quantidade de convites.
+ * @return 
+ */
+    public int getQuatConvites() {
+        return quatConvites;
+    }
+/**
+ * Metodo setQuatConvites que permite alterar a quantidade de convites.
+ * @param quatConvites 
+ */
+    public void setQuatConvites(int quatConvites) {
+        this.quatConvites = quatConvites;
+    }
+/**
+ * Metodo getNomeEmp que retorna o nome da empresa.
+ * @return 
+ */
+    public String getNomeEmp() {
+        return nomeEmp;
+    }
+/**
+ * Metodo setNomeEmp que permite alterar o nome da empresa.
+ * @param nomeEmp 
+ */
+    public void setNomeEmp(String nomeEmp) {
+        this.nomeEmp = nomeEmp;
+    }
+/**
+ * Metodo getMorada que retorna a Morada.
+ * @return 
+ */
+    public String getMorada() {
+        return morada;
+    }
+/**
+ * Metodo setMorada que permite alterar a morada.
+ * @param morada 
+ */
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+/**
+ * Metodo getTelefone que retorna o telefone.
+ * @return 
+ */
+    public int getTelefone() {
+        return telefone;
+    }
+/**
+ * Metodo setTelefone que permite alterar o telefone.
+ * @param telefone 
+ */
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
     }
           
     
+          
     
     
 }
